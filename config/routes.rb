ecaa7df75256754ee5610200544e69c5fd3cbdee
Anton Zaharia <zaharia_anton@yahoo.com>
2021-09-14 13:57:16 +0100

    auth system complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root to: "home#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
 end
